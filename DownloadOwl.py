753763c4946e2f10502adb571597ec89135b50fb
Cam Schriever <>
2024-06-14 15:47:59 +0930

    Fixed reddit json query. Added requirements.txt

--- DownloadOwl.py
+++ DownloadOwl.py
@@ -5,21 +5,30 @@ from typing import final
 import requests
 
 def DownloadLatestImage(subreddit):
-    r = requests.get(''.join(['https://www.reddit.com/r/', subreddit,'/new/.json']), headers = {'User-agent': 'InkyOwl'})
+    response = requests.get(''.join(['https://www.reddit.com/r/', subreddit,'/new/.json']), headers = {'User-agent': 'InkyOwl'})
     keyval = "data"
-    data = r.json()
+    data = response.json()
     url = ""
     pos = -1 #seeding to start in first position
     
     #reddit automatically converts all images to jpg
     urlIsValid = False
     while not (urlIsValid):
-        print(url)
+        print("Attempt {}: {}".format(pos, url))
         pos+= 1
-        url = data[keyval]['children'][pos]['data']['url_overridden_by_dest'] #Errors out if the key doesnt exist. Eg. the image is a gallery.
+        
+        if pos > 10: #don't make too many attempts
+            print("Couldn't find a suitable image")
+            break
+        
+        # try loading the current url info.
+        try:
+            url = data[keyval]['children'][pos]['data']['url_overridden_by_dest'] #Errors out if the key doesnt exist. Eg. the image is a gallery.
+        except:
+            continue
         
         #check if it already is an image
-        if not url.endswith(('.jpg')):
+        if not url.endswith(('.jpeg')):
             #else, try getting the first image of an album
             try:
                 url = "https://i.redd.it/" + data[keyval]['children'][pos]['data']['gallery_data']['items'][0]['media_id'] + ".jpg"
@@ -30,11 +39,9 @@ def DownloadLatestImage(subreddit):
         response = requests.get(url)
         if response.status_code == 200:
             urlIsValid = True
-            with open(''.join([subreddit, ".jpg"]), 'wb') as f:
+            with open(''.join([subreddit, ".jpeg"]), 'wb') as f:
                 f.write(response.content)
         
         
-        if pos > 10: #don't make too many attempts
-            break
 
 DownloadLatestImage('aww')
